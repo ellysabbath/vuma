01c4bbe5736db725ab7b37ba3b139641d949f8ab
unknown <mwananjelaelisha36@gmail.com>
2026-06-25 08:53:52 +0300

    commits with SEO

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,52 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     host: '0.0.0.0',
+//     port: 3000,
+//     open: true
+//   },
+//   // Add these for Vercel deployment
+//   build: {
+//     outDir: 'dist',
+//     sourcemap: false,
+//     rollupOptions: {
+//       output: {
+//         manualChunks: undefined
+//       }
+//     }
+//   },
+//   base: '/' // Important for Vercel
+// })
+
+
+
+
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { Sitemap } from 'vite-plugin-sitemap'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    Sitemap({
+      hostname: 'https://vumatanzania.or.tz',
+      routes: [
+        '/',
+        '/login',
+        '/signup'
+      ]
+    })
+  ],
   server: {
     host: '0.0.0.0',
     port: 3000,
     open: true
   },
-  // Add these for Vercel deployment
   build: {
     outDir: 'dist',
     sourcemap: false,
@@ -18,5 +56,5 @@ export default defineConfig({
       }
     }
   },
-  base: '/' // Important for Vercel
+  base: '/'
 })
