856f649e3387f39f06fff368ca17e653fe8cfba9
unknown <mwananjelaelisha36@gmail.com>
2026-06-25 06:00:40 +0300

    initial commit for production

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ import Report from './pages/Report';
 import Apply from './pages/Apply';
 import EventRegister from './pages/EventRegister';
 import Leadership from './pages/Leadership';
-import Pro from './components/programs'
+
 import EventRegistrationConfirmation from './pages/EventRegistrationConfirmation';
 import Publications from './pages/Publications';
 import PublicationDetails from './pages/PubDetails';
